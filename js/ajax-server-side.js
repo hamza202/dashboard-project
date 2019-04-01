0dea294a3677446c824b118caf4657b07e85310c
hamza202 <k8668975@gmail.com>
2019-04-01 03:34:31 +0300

    datatble

---
+++ js/ajax-server-side.js
@@ -0,0 +1,37 @@
+var DatatablesDataSourceAjaxServer = {
+    init: function () {
+        $("#m_table_1").DataTable({
+            responsive: !0,
+            searchDelay: 500,
+            processing: !0,
+            dom: '"<\'row\'<\'col-sm-12 col-md-4\'l><\'col-sm-12 col-md-4\'B><\'col-sm-12 col-md-4\'f>>" +\n' +
+            '"<\'row\'<\'col-sm-12\'tr>>" +\n' +
+            '"<\'row\'<\'col-sm-12 col-md-5\'i><\'col-sm-12 col-md-7\'p>>",',
+            buttons: [
+                'print', 'excel', 'pdf'
+            ],
+            "ordering": false,
+            oLanguage: {
+                oPaginate: {
+                    sNext: '<span class="pagination-default"></span><span class="pagination-fa"><i class="fa fa-chevron-right" ></i></span>',
+                    sPrevious: '<span class="pagination-default"></span><span class="pagination-fa"><i class="fa fa-chevron-left" ></i></span>'
+                },
+                "sSearch": "<span>Search all fields:</span> _INPUT_"
+            },
+            serverSide: !0,
+            ajax: "https://keenthemes.com/metronic/themes/themes/metronic/dist/preview/inc/api/datatables/demos/server.php",
+            columns: [{data: "OrderID"}, {data: "Country"}, {data: "ShipCity"}, {data: "Status"}, {data: "Type"}, {data: "Actions"}],
+            columnDefs: [{
+                targets: -1, orderable: !1, render: function (a, e, t, n) {
+                    return '<div class="custom-control custom-checkbox mr-sm-2">\n' +
+                        '                        <input type="checkbox" class="custom-control-input" id="check2">\n' +
+                        '                        <label class="custom-control-label" for="check2"></label>\n' +
+                        '                    </div>'
+                }
+            }]
+        })
+    }
+};
+jQuery(document).ready(function () {
+    DatatablesDataSourceAjaxServer.init()
+});
