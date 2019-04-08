b59ba84b5ecaa5912bc3cab8a46053504798e40b
hamza202 <k8668975@gmail.com>
2019-04-08 08:35:50 +0300

    table edit

--- js/ajax-server-side.js
+++ js/ajax-server-side.js
@@ -4,7 +4,7 @@ var DatatablesDataSourceAjaxServer = {
             responsive: !0,
             searchDelay: 500,
             processing: !0,
-            dom: '"<\'row\'<\'col-sm-12 col-md-4\'l><\'col-sm-12 col-md-4\'B><\'col-sm-12 col-md-4\'f>>" +\n' +
+            dom: '"<\'row\'<\'col-sm-12 col-md-3\'l><\'col-sm-12 col-md-5\'B><\'col-sm-12 col-md-4\'f>>" +\n' +
             '"<\'row\'<\'col-sm-12\'tr>>" +\n' +
             '"<\'row\'<\'col-sm-12 col-md-5\'i><\'col-sm-12 col-md-7\'p>>",',
             buttons: [
@@ -23,15 +23,18 @@ var DatatablesDataSourceAjaxServer = {
             columns: [{data: "OrderID"}, {data: "Country"}, {data: "ShipCity"}, {data: "Status"}, {data: "Type"}, {data: "Actions"}],
             columnDefs: [{
                 targets: -1, orderable: !1, render: function (a, e, t, n) {
-                    return '<div class="custom-control custom-checkbox mr-sm-2">\n' +
-                        '                        <input type="checkbox" class="custom-control-input" id="check2">\n' +
-                        '                        <label class="custom-control-label" for="check2"></label>\n' +
-                        '                    </div>'
+                    return '<div class="custom-control  custom-checkbox mr-sm-2 d-inline-block check-inc">\n' +
+                        '                        <input type="checkbox" class="custom-control-input cheksub" >\n' +
+                        '                        <label class="custom-control-label" ></label>\n' +
+                        '                    </div>'+
+                        '<a href="#" class="d-inline-block view-link">View</a>'
                 }
             }]
         })
     }
+
 };
 jQuery(document).ready(function () {
     DatatablesDataSourceAjaxServer.init()
 });
+
